6146972e4caa9bb4e40bd561b1f92bb24787a2b2
Peter-Welsh <peter.t.welsh@pm.me>
2024-11-29 11:38:08 -0600

    Add solution for MergedSortedArray

---
+++ Exercises/Solutions/MergeSortedArray.cs
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/merge-sorted-array/description/
+namespace Exercises.Solutions;
+
+public class MergeSortedArray
+{
+    // Understandable and concise solution (for use when performance is not the top priority)
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (var i = 0; i < n; i++)
+        {
+            nums1[m] = nums2[i];
+            m++;
+        }
+        Array.Sort(nums1);
+    }
+    
+    // Ugly solution -- use only when performance is the top priority
+    public void Merge_Ugly(int[] nums1, int m, int[] nums2, int n)
+    {
+        var i = m - 1;
+        var j = n - 1;
+        var k = m + n - 1;
+        while (j >= 0)
+        {
+            if (i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+}
