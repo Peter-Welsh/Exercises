b3840115f3ed7304e0d6c3431268fde476afa7ea
Peter-Welsh <peter.t.welsh@pm.me>
2024-12-02 15:23:23 -0600

    Add solution for RotateArray

---
+++ Exercises/Solutions/Array or String/RotateArray.cs
@@ -0,0 +1,12 @@
+namespace Exercises.Solutions.Array_or_String;
+
+public class RotateArray
+{
+    public void Rotate(int[] nums, int k)
+    {
+        var pivot = k % nums.Length;
+        Array.Reverse(nums);
+        Array.Reverse(nums, 0, pivot);
+        Array.Reverse(nums, pivot, nums.Length - pivot);
+    }
+}
