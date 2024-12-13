0e393f0db265b0d041cbcf3025185e797db610c7
Peter-Welsh <peter.t.welsh@pm.me>
2024-12-12 19:13:38 -0600

    Add solution for TwoSum

--- Exercises/Solutions/Hashmap/Two_Sum.cs
+++ Exercises/Solutions/Hashmap/Two_Sum.cs
@@ -3,6 +3,9 @@ namespace Exercises.Solutions.Hashmap;
 
 public class Two_Sum
 {
+    /// <summary>
+    /// Fast solution - O(n) 
+    /// </summary>
     public int[] TwoSum(int[] nums, int target)
     {
         var indexesMap = new Dictionary<int, int>();
@@ -13,6 +16,28 @@ public class Two_Sum
                 return [index, i];
             }
             indexesMap[nums[i]] = i;
+        }
+        return [];
+    }
+    
+    /// <summary>
+    /// Slow solution - O(n^2) 
+    /// </summary>
+    public int[] TwoSum_Slow(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+                if (nums[j] + nums[j - 1] == target)
+                {
+                    return [j, j-1];
+                }
+            }
         }
         return [];
     }
